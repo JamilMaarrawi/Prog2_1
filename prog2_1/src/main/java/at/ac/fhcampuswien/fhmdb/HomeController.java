03018194b19fc1d33385a8b65b2d3858e2abe096
JamilMaarrawi <146196852+JamilMaarrawi@users.noreply.github.com>
2024-03-11 23:06:07 +0100

    Add files via upload

---
+++ prog2_1/src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -0,0 +1,82 @@
+package at.ac.fhcampuswien.fhmdb;
+
+import at.ac.fhcampuswien.fhmdb.models.Genre;
+import at.ac.fhcampuswien.fhmdb.models.Movie;
+import at.ac.fhcampuswien.fhmdb.ui.MovieCell;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventType;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.*;
+
+public class HomeController implements Initializable {
+    @FXML
+    public JFXButton searchBtn;
+
+    @FXML
+    public TextField searchField;
+
+    @FXML
+    public JFXListView movieListView;
+
+    @FXML
+    public JFXComboBox genreComboBox;
+
+    @FXML
+    public JFXButton sortBtn;
+
+    public List<Movie> allMovies = Movie.initializeMovies();
+    public List<Genre> allGenres = new ArrayList<>(EnumSet.allOf(Genre.class));    /// Creates a List with all genres
+
+    private  ObservableList<Movie> observableMovies = FXCollections.observableArrayList();   // automatically updates corresponding UI elements when underlying data changes
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //observableMovies.addAll(allMovies);         // add dummy data to observable list
+
+        // initialize UI stuff
+        movieListView.setItems(observableMovies);   // set data of observable list to list view
+        movieListView.setCellFactory(movieListView -> new MovieCell()); // use custom cell factory to display data
+
+        // TODO add genre filter items with genreComboBox.getItems().addAll(...)
+        genreComboBox.setPromptText("Filter by Genre");
+        genreComboBox.getItems().addAll(allGenres);    /// adds genre-list to dropbox
+
+        // TODO add event handlers to buttons and call the regarding methods
+        // either set event handlers in the fxml file (onAction) or add them here
+
+        // Sort button example:
+        sortBtn.setOnAction(actionEvent -> {
+            if(sortBtn.getText().equals("Sort (asc)")) {
+                // TODO sort observableMovies ascending
+                observableMovies.sort(Comparator.comparing(Movie::getTitle)); /// sort list alphabetically
+
+                sortBtn.setText("Sort (desc)");
+            } else {
+                // TODO sort observableMovies descending
+                observableMovies.sort(Comparator.comparing(Movie::getTitle).reversed()); /// sort list alphabetically reversed
+
+                sortBtn.setText("Sort (asc)");
+            }
+        });
+
+        searchBtn.setOnAction(actionEvent -> {
+            observableMovies.removeAll(allMovies);
+            for (Movie m : allMovies) {
+                if(m.getGenres().contains(genreComboBox.getValue()))
+                    observableMovies.add(m);
+            }
+            movieListView.setCellFactory(movieListView -> new MovieCell());
+        });
+
+
+    }
+}
---
+++ prog2_1/src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -0,0 +1,67 @@
+package at.ac.fhcampuswien.fhmdb.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    private String title;
+    private String description;
+    // TODO add more properties here
+    private List<Genre> genres; /// adds genre property
+
+    public Movie(String title, String description,List<Genre> genres) {
+        this.title = title;
+        this.description = description;
+        this.genres = genres; /// Constructor with genres
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    } /// Getter method genres
+
+    public static List<Movie> initializeMovies(){
+        List<Movie> movies = new ArrayList<>();
+        // TODO add some dummy data here
+        /// Dummy Dara
+        List<Genre> lifeIsBeautifulGenres = new ArrayList<>();
+        lifeIsBeautifulGenres.add(Genre.DRAMA);
+        lifeIsBeautifulGenres.add(Genre.ROMANCE);
+        movies.add(new Movie("Life is Beautiful", "When an open-minded Jewish librarian and his son become victims of the Holocaust, he uses a perfect mixture of will, humor, and imagination to protect his son from the dangers around their camp.", lifeIsBeautifulGenres));
+
+        List<Genre> pussInBootsGenres = new ArrayList<>();
+        pussInBootsGenres.add(Genre.COMEDY);
+        pussInBootsGenres.add(Genre.FAMILY);
+        pussInBootsGenres.add(Genre.ANIMATION);
+        movies.add(new Movie("Puss in Boots", "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which begin when five criminals meet at a seemingly random police lineup.", pussInBootsGenres));
+
+        List<Genre> usualSuspectsGenres = new ArrayList<>();
+        usualSuspectsGenres.add(Genre.CRIME);
+        usualSuspectsGenres.add(Genre.DRAMA);
+        usualSuspectsGenres.add(Genre.MYSTERY);
+        movies.add(new Movie("The Usual Suspects", "An outlaw cat, his childhood egg-friend, and a seductive thief kitty set out in search for the eggs of the fabled Golden Goose to clear his name, restore his lost honor, and regain the trust of his mother and town.", usualSuspectsGenres));
+
+        List<Genre> avatarGenres = new ArrayList<>();
+        avatarGenres.add(Genre.ANIMATION);
+        avatarGenres.add(Genre.DRAMA);
+        avatarGenres.add(Genre.ACTION);
+        movies.add(new Movie("Avatar", "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.", avatarGenres));
+
+        List<Genre> wolfOfWallStreetGenres = new ArrayList<>();
+        wolfOfWallStreetGenres.add(Genre.DRAMA);
+        wolfOfWallStreetGenres.add(Genre.ROMANCE);
+        wolfOfWallStreetGenres.add(Genre.BIOGRAPHY);
+        movies.add(new Movie("The Wolf of Wall Street", "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.", wolfOfWallStreetGenres));
+
+        return movies;
+    }
+
+
+}
